5927e800291267c4c4c596a551bbe408822c126b
AlexanderBlom <31540337+AlexanderBlom@users.noreply.github.com>
2018-03-02 22:17:38 +0100

    Add files via upload

--- spel.js
+++ spel.js
@@ -1,13 +1,17 @@
 /* element för manipulation */
 var button = document.getElementById("clickerbutton");
-var lionButton = document.getElementById("lion");
+var lizardButton = document.getElementById("lizard");
 var scoreDiv = document.getElementById("score");
 var powerText = document.getElementById("powerText");
 var zebraButton = document.getElementById("zebra");
 var birdButton = document.getElementById("bird");
-var lionCounter = document.getElementById("lionCounter");
+var lizardCounter = document.getElementById("lizardCounter");
 var zebraCounter = document.getElementById("zebraCounter");
 var birdCounter = document.getElementById("birdCounter");
+
+var odla;
+var zebran;
+var fageln;
 
 /* Skapa ett nytt element för poängen */
 var scoreText = document.createElement("p");
@@ -15,20 +19,23 @@ var scoreText = document.createElement("p");
 /* spelvariabler */
 var clickValue = 1;
 var bank = 0;
-var lionCost = 150;
+var lizardCost = 150;
 var zebraCost = 30;
 var zebra = null;
 var birdCost = 100;
-var lionCount = 0;
+var lizardCount = 0;
 var zebraCount = 0;
 var birdCount = 0;
+var odlai = 0;
+var zebrai = 0;
+var fageli = 0;
 
 /* startvärden */
 scoreText.textContent = "Points: 0";
-lionCounter.textContent = "Lejon: 0";
+lizardCounter.textContent = "Ödlor: 0";
 zebraCounter.textContent = "Zebror: 0";
 birdCounter.textContent = "Fåglar: 0";
-lionButton.textContent = "Lejon: " + lionCost;
+lizardButton.textContent = "Ödla: " + lizardCost;
 zebraButton.textContent = "Zebra: " + zebraCost;
 birdButton.textContent = "Fågel: " + birdCost;
 
@@ -38,19 +45,32 @@ button.addEventListener("click", function() {
 	scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till bank.
 }, true);
 
-// knapp och kod för lion powerup
-lionButton.addEventListener("click", function() {
-	if (bank >= lionCost) {
+// knapp och kod för lizard powerup
+lizardButton.addEventListener("click", function() {
+	if (bank >= lizardCost) {
 		clickValue += 1;
-		bank -= lionCost;
-		lionCost += 200;
-		lionButton.textContent = "Lejon: " + lionCost;
-		powerText.textContent = "Köpte lion";
-		lionCount += 1;
+		bank -= lizardCost;
+		lizardCost += 200;
+		lizardButton.textContent = "Ödlor: " + lizardCost;
+		powerText.textContent = "Köpte Ödla";
+		lizardCount += 1;
 		scoreText.textContent = "Points: " + bank; // sätt textvärdet i p elementet till 
-		lionCounter.textContent = "Lejon: " + lionCount;
+		lizardCounter.textContent = "Ödla: " + lizardCount;
+		
+		odla = document.createElement("div");
+		odla.className = "odlan";
+		document.getElementsByTagName("body")[0].appendChild(odla);
+		odlai++;
+
+		if ((odlai % 2) == 1) {
+			odla.style.transform = "scaleX(-1)"
+		}
+		odla.style.top = Math.random()*1000 + "px";
+		odla.style.left = Math.random()*1300 + "px";
+		odla.style.right = Math.random()*1300 + "px";
+		odla.style.bottom = Math.random()*1000 + "px";
 	} else {
-		powerText.textContent = "Du har inte råd med lion";
+		powerText.textContent = "Du har inte råd med Ödla";
 	}
 }, true);
 
@@ -60,7 +80,22 @@ zebraButton.addEventListener("click", function() {
 		bank -= zebraCost;
 		powerText.textContent = "Köpte zebra";
 		zebraCount += 1;
+		zebraCost = Math.floor(zebraCost * 1.25);
 		zebraCounter.textContent = "Zebror: " + zebraCount;
+		zebraButton.textContent = "Zebra : " + zebraCost;
+		
+		zebran = document.createElement("div");
+		zebran.className = "zebran";
+		document.getElementsByTagName("body")[0].appendChild(zebran);
+
+		zebrai++;
+		if ((zebrai % 2) == 1) {
+			zebran.style.transform = "scaleX(-1)"
+		}
+		zebran.style.top = Math.random()*1000 + "px";
+		zebran.style.left = Math.random()*1300 + "px";
+		zebran.style.right = Math.random()*1300 + "px";
+		zebran.style.bottom = Math.random()*1000 + "px";
 		// Lägg till setInterval med en funktion som laddas varje sekund
 		zebra = setInterval(function() {
 			bank += 2;
@@ -76,6 +111,21 @@ birdButton.addEventListener("click",function(){
 		powerText.textContent = "Köpte Fågel";
 		birdCount += 1;
 		birdCounter.textContent = "Fåglar: " + birdCount;
+		birdCost = Math.floor(birdCost * 1.5);
+		birdButton.textContent = "Fåglar: " + birdCost;
+
+		fageln = document.createElement("div");
+		fageln.className = "fageln";
+		document.getElementsByTagName("body")[0].appendChild(fageln);
+
+		fageli++;
+		if ((fageli % 2) == 1) {
+			fageln.style.transform = "scaleX(-1)"
+		}
+		fageln.style.top = Math.random()*1000 + "px";
+		fageln.style.left = Math.random()*1300 + "px";
+		fageln.style.right = Math.random()*1300 + "px";
+		fageln.style.bottom = Math.random()*1000 + "px";
 		// Lägg till setInterval med en funktion som laddas varje sekund
 		bird = setInterval(function() {
 			bank += 5;
